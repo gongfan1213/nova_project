bac30cc2630f2ea1162225e597fae8eb5e641686
gongfan1213 <gongfan1213@163.com>
2025-06-10 10:30:42 +0800

    暂时隐藏部分按钮

--- apps/web/src/components/chat-interface/thread.tsx
+++ apps/web/src/components/chat-interface/thread.tsx
@@ -92,9 +92,9 @@ export const Thread: FC<ThreadProps> = (props: ThreadProps) => {
 
   return (
     <ThreadPrimitive.Root className="flex flex-col h-full w-full">
-      <div className="pr-3 pl-6 pt-3 pb-2 flex flex-row gap-4 items-center justify-between">
+      {/* <div className="pr-3 pl-6 pt-0 pb-2 flex flex-row gap-4 items-center justify-between">
         <div className="flex items-center justify-start gap-2 text-gray-600">
-          {/* <ThreadHistory
+           <ThreadHistory
             switchSelectedThreadCallback={switchSelectedThreadCallback}
           />
           <TighterText className="text-xl">Nova</TighterText>
@@ -106,8 +106,8 @@ export const Thread: FC<ThreadProps> = (props: ThreadProps) => {
               setModelConfig={setModelConfig}
               modelConfigs={modelConfigs}
             />
-          )} */}
-        </div>
+          )} 
+        </div> 
         {hasChatStarted ? (
           <div className="flex flex-row flex-1 gap-2 items-center justify-end">
             <TooltipIconButton
@@ -134,7 +134,7 @@ export const Thread: FC<ThreadProps> = (props: ThreadProps) => {
             <ReflectionsDialog selectedAssistant={selectedAssistant} />
           </div>
         )}
-      </div>
+      </div> */}
       <ThreadPrimitive.Viewport className="flex-1 overflow-y-auto scroll-smooth bg-inherit px-4 pt-8">
         {!hasChatStarted && (
           <ThreadWelcome
