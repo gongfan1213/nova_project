5c06b485a12977b949ae1495ed49f07b61555b4e
hans <im.hansking@gmail.com>
2025-06-16 19:20:43 +0800

    fix: 输出流

--- apps/web/src/contexts/GraphContext.tsx
+++ apps/web/src/contexts/GraphContext.tsx
@@ -399,13 +399,6 @@ export function GraphProvider({ children }: { children: ReactNode }) {
 
                   if (data.answer) {
                     artifactContent += data.answer;
-                    // 如果有 <<think>xxx</think> 则只保留 <message>xxx</message>中的内容，则去掉 <<think>xxx</think>和<message>xxx</message>
-                    followupContentRef.current =
-                      followupContentRef.current.replace(
-                        /<think>[\s\S]*<\/think>[\s\S]*<message>[\s\S]*<\/message>/g,
-                        ""
-                      );
-
                     // 实时更新artifact显示
                     const newArtifact: ArtifactV3 = {
                       currentIndex: 1,
@@ -422,6 +415,13 @@ export function GraphProvider({ children }: { children: ReactNode }) {
                     if (!firstTokenReceived) {
                       setFirstTokenReceived(true);
                     }
+                    setAgentMessage(
+                      followupContentRef,
+                      followupMessageId,
+                      finalMessages,
+                      finalFunctionTools,
+                      data
+                    );
                     setArtifact(newArtifact);
                     finalArtifact = newArtifact;
                   }
@@ -583,6 +583,13 @@ export function GraphProvider({ children }: { children: ReactNode }) {
                   if (!firstTokenReceived) {
                     setFirstTokenReceived(true);
                   }
+                  setAgentMessage(
+                    followupContentRef,
+                    followupMessageId,
+                    finalMessages,
+                    finalFunctionTools,
+                    data
+                  );
                   setArtifact(newArtifact);
                 }
               } catch (e) {
@@ -1030,11 +1037,15 @@ export function GraphProvider({ children }: { children: ReactNode }) {
     finalFunctionTools: any,
     data: any
   ) => {
-    // if(data.answer){
-    //   followupContentRef.current = followupContentRef.current + data.answer;
-    // }
-    data.thought &&
-      (followupContentRef.current = followupContentRef.current + data.thought);
+    console.log("hansking-fc-data", data.tool_input);
+    if(data.answer){
+    console.log("hansking-message-data", data.answer);
+      followupContentRef.current = followupContentRef.current + data.answer;
+    }
+
+    // data.thought &&
+    //   (followupContentRef.current = followupContentRef.current + data.thought);
+
     // 需要替换掉 <artifact>xxx</artifact>
     followupContentRef.current = followupContentRef.current.replace(
       /<artifact>[\s\S]*<\/artifact>/g,
